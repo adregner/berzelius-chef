e5ad5708ac2db38f80cd695b71a9fd604b32a3d1
Andrew Regner <andrew@aregner.com>
2013-06-28 10:07:35 -0400

    does this work...

--- cookbooks/berzelius/metadata.rb
+++ cookbooks/berzelius/metadata.rb
@@ -1,6 +1,7 @@
-name 'berzelius'
-maintainer 'Andrew Regner'
+name             'berzelius'
+maintainer       'Andrew Regner'
 maintainer_email 'andrew.regner@mailtrust.com'
-license 'MIT'
-description ''
-version '0.1.0'
+license          'All rights reserved'
+description      'Installs/Configures a node as part of the Berzelius system.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
