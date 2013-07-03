f656718045f5acf88ac2a10892d5948f8ee6a794
Andrew Regner <andrew@aregner.com>
2013-07-03 10:43:49 -0400

    add beginning of api-web-db recipe and knife solo things

--- cookbooks/berzelius/metadata.rb
+++ cookbooks/berzelius/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures a node as part of the Berzelius system.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends          'mongodb-10gen' # for the API / Web / DB node
---
+++ cookbooks/berzelius/recipes/api-web-db.rb
@@ -0,0 +1,2 @@
+include_recipe "mongodb-10gen::single"
+
